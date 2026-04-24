c45f6b9218a18837322dd2da0f56656ae59ae4f3
santiago de hoyos <santiagodehoyoshernandez@.net>
2026-04-23 19:35:24 -0500

    se guardo la documentacion

---
+++ documentacion_files/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"caballo"},{"p":"<Unnamed>","l":"principal"},{"p":"<Unnamed>","l":"principal.Principal"}];updateSearchResults();
